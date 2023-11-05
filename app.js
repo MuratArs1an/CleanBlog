aa036129392fc7bfa6e0f114fc56c3760b343a67
MuratArs1an <kilicarslan87@gmail.com>
2023-11-05 22:46:30 +0300

    templated engine added

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 const express=require('express');
 const app=express();
+const ejs=require('ejs');
 const port=3000;
 
 app.listen(port,()=>{
     console.log(`Sunucu ${port} unda Başlatıldı`);
 });
 
+//template engine
+app.set('view engine', 'ejs');
 
+//middleware
+app.use(express.static('public'));
 
 app.get('/',(req,res)=>{
+    res.render('index')
+})
 
-    const blog= {
-        id: 1, 
-        title: "Blog title", 
-        description: "Blog description" 
-    };
+app.get('/about',(req,res)=>{
+    res.render('about')
+})
 
-    res.send(blog)
-});
-
+app.get('/add_post',(req,res)=>{
+    res.render('add_post')
+})
